f0e618eba58b36c7a08f8276de8126ec3fa599f6
sahulakki123 <lakkisahus04@gmail.com>
2025-12-05 16:20:13 +0530

    update

--- src/Form_Handeling.jsx
+++ src/Form_Handeling.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from 'react'
-import './Te.css'
 
 const Form = () => {
 
-    let [name, setname] = useState("")
-    let [email, setemail] = useState("")
-    let [num, setnum] = useState("")
+    // let [name, setname] = useState("")
+    // let [email, setemail] = useState("")
+    // let [num, setnum] = useState("")
+
+    let [form, setform] = useState({
+      name : "",
+      email : "",
+      number : "",
+    })
+     
+    let handlingchange=(e)=>{
+      setform({ ...form , [e.target.name] : e.target.value })
+    }
+
 
 
     let Handeling=(e)=>{
-
         e.preventDefault()
+        if (form.name.trim()==""){
 
-        alert([name, email, num])
+          alert("Name cannot be empty")
+        }
+
     }
 
 
@@ -22,10 +34,10 @@ const Form = () => {
 
     <h1>Form_Handeling </h1>
 
-    <form onSubmit={Handeling} className=''>
-        Enter Name : <input type="text" value={name} onChange={(e)=>{setname(e.target.value)}} /> <br /> <br />
-        Enter Email : <input type="text" value={email} onChange={(e)=>{setemail(e.target.value)}} /> <br /> <br />
-        Enter Number : <input type="number" value={num} onChange={(e)=>{setnum(e.target.value)}} /> <br /> <br />
+    <form onSubmit={Handeling}>
+        Enter Name : <input type="text" name="name" value={form.name} onChange={handlingchange} /> <br /> <br />
+        Enter Email : <input type="text" name="email" value={form.email} onChange={handlingchange} /> <br /> <br />
+        Enter Number : <input type="text" name="number" value={form.number} onChange={handlingchange} /> <br /> <br />
 
         <button type='submit'>Save Data</button>
     </form>
